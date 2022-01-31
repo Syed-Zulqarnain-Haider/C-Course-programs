e8d9faddd2d5262972b0b14f0ac99af5d09a811f
Syed-Zulqarnain-Haider <97968358+Syed-Zulqarnain-Haider@users.noreply.github.com>
2022-01-31 08:51:45 -0800

    Program

---
+++ WEEK 2.0/Megabyte_to_Bit.cpp
@@ -0,0 +1,12 @@
+/*In this program i will take input from user to convert megabyte into bit.*/
+
+#include <iostream>
+using namespace std;
+main()
+{
+    int mega=0, bit=0 ;
+    cout << "Enter the Megabytes you want to convert into bits : ";
+    cin >> mega;
+    bit = mega * 1024 * 1024 * 8;
+    cout << bit <<endl;
+}
